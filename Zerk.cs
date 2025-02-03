5e4b3b17a9729f701f13df15d44ee25a406f6c2b
Adam Sranko <srankoadam@gmail.com>
2025-02-03 20:14:12 +0100

    Actual Zerk anims etc

--- Zerk.cs
+++ Zerk.cs
@@ -7,67 +7,75 @@ public partial class Zerk : Area2D
     private SkillController _skillController;
     public override void _Ready()
 	{
-        //var animPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
-        //var idleSprite = GetNode<Sprite2D>("Idle");
+        var animPlayer = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
 
-        //_skillController = SkillController.CreateSkillController(animPlayer, idleSprite);
+        _skillController = SkillController.InitSkillController(animPlayer);
+        _skillController.CastSpeedScale = 1f;
 
-        //var thunderSprite = GetNode<Sprite2D>("ThunderSprite");
-        //PlayerSkill skil1 = new PlayerSkill("thunder",2000, "thunderstrike", thunderSprite);
-        //skil1.IsChargable = false;
+        // CHAINABLE SKILL
+        PlayerSkill chainSourceSkill = new PlayerSkill("chain-source", 2000, "skill_2");
+        PlayerSkill chainedIntoSkill_chained = new PlayerSkill("chained-into", 1000, "NONE");
+        PlayerSkill chainedIntoSkill_original = new PlayerSkill("not-chained-into", 2000, "skill_3");
+        chainSourceSkill.CreateSkillChainTo(chainedIntoSkill_original, chainedIntoSkill_chained, 1000);
+        chainSourceSkill.MakeSkillCancellable(1500);
+        // CHAINABLE SKILL
 
-        //var blockSprite = GetNode<Sprite2D>("BlockSprite");
-        //PlayerSkill skilBlock = new PlayerSkill("block", 999999, "block", blockSprite);
-        //skilBlock.IsChargable = true;
+        // CHARGABLE SKILL
+        PlayerSkill chargingSkill1 = new PlayerSkill("charging", 999999, "skill_1");
+        chargingSkill1.MakeSkillCancellable();
 
-        //_skillController.RegisterSkill(skil1);
-        //_skillController.RegisterAnimCancellingSkill(skilBlock);
+        PlayerSkill chargeIntoSkill = new PlayerSkill("charge-into", 1000, "NONE");
+        chargingSkill1.ConvertSkillToCharging(chargeIntoSkill);
+        // CHARGABLE SKILL
+
+        // ANIM CANCELLING BLOCK
+        PlayerSkill skilBlock = new PlayerSkill("block", 999999, "block");
+        skilBlock.IsChargable = true;
+        // ANIM CANCELLING BLOCK
+
+        _skillController.RegisterSkill(chainSourceSkill);
+        _skillController.RegisterSkill(chainedIntoSkill_original);
+        _skillController.RegisterSkill(chargingSkill1);
+
+
+        _skillController.RegisterAnimCancellingSkill(skilBlock);
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        //_skillController._Process(delta);
-        //var velocity = Vector2.Zero; // The player's movement vector.
+        _skillController._Process(delta);
 
-        //if (Input.IsActionPressed("move_right"))
-        //{
-        //    velocity.X += 1;
-        //}
+        var velocity = Vector2.Zero;
+        if (Input.IsActionPressed("move_right"))
+        {
+            velocity.X += 1;
+        }
 
-        //if (Input.IsActionPressed("move_left"))
-        //{
-        //    velocity.X -= 1;
-        //}
+        if (Input.IsActionPressed("move_left"))
+        {
+            velocity.X -= 1;
+        }
 
-        //if (Input.IsActionPressed("move_down"))
-        //{
-        //    velocity.Y += 1;
-        //}
+        if (Input.IsActionPressed("move_down"))
+        {
+            velocity.Y += 1;
+        }
 
-        //if (Input.IsActionPressed("move_up"))
-        //{
-        //    velocity.Y -= 1;
-        //}
-        ////if (Input.IsActionPressed("thunderstrike"))
-        ////{
-        ////    _animPlayer.Play("thunder");
-        ////}
-        ////if (Input.IsActionPressed("block"))
-        ////{
-        ////    //_animPlayer.Play("block");
-        ////}
-
-        //if (velocity.Length() > 0)
-        //{
-        //    velocity = velocity.Normalized() * 200;
+        if (Input.IsActionPressed("move_up"))
+        {
+            velocity.Y -= 1;
+        }
+        if (velocity.Length() > 0)
+        {
+            velocity = velocity.Normalized() * 200;
 
 
-        //    Position += velocity * (float)delta;
-        //    Position = new Vector2(
-        //        x: Mathf.Clamp(Position.X, 0, 1920),
-        //        y: Mathf.Clamp(Position.Y, 0, 1080)
-        //    );
-        //}
+            Position += velocity * (float)delta;
+            Position = new Vector2(
+                x: Mathf.Clamp(Position.X, 0, 1920),
+                y: Mathf.Clamp(Position.Y, 0, 1080)
+            );
+        }
     }
 }
